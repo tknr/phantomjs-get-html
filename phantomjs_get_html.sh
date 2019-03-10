e446290dd33e859d1b160b0a1a03ce5f4e7f2d44
tknr <geishagirlskoume@gmail.com>
2019-03-10 09:38:13 +0900

    2019年  3月 10日 日曜日 09:38:13 JST

--- phantomjs_get_html.sh
+++ phantomjs_get_html.sh
@@ -5,8 +5,10 @@ DATE=`date +%Y_%m_%d`
 argv=("$@")
 CMDNAME=`basename $0`
 
+echo `readlink -f .`
+
 if [ $# -eq 0 ]; then
-	echo "Usage : ${CMDNAME} [dirname]"
+	echo "Usage : ${CMDNAME} [url]"
 	exit 1
 fi
 
