50721916d6a7fe0411ad4a3b77a8893a9d0f185d
tknr <geishagirlskoume@gmail.com>
2020-04-07 06:37:43 +0900

    2020年  4月  7日 火曜日 06:37:42 JST

--- proxy.sh
+++ proxy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -x
 cd `dirname $0`
 ~/src/git/phantomjs-get-html/cybersyndrome.sh | grep ":80" > proxy.txt
+~/src/git/phantomjs-get-html/cybersyndrome.sh | grep ":3128" > proxy.txt
 ~/src/git/phantomjs-get-html/cybersyndrome.sh | grep ":443" | sed -e "s/http/https/g" >> proxy.txt
